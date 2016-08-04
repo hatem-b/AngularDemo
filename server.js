259b216b35917f6a7aef630628a2d1fd47f05ff3
Hatem Belaid <belaid.h@sfeir.com>
2016-08-04 14:04:32 +0200

    :bug: fix url routes on server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('*', function(request, response, next) {
+  response.sendFile(__dirname + '/dist/index.html');
+});
+
 app.set('port', process.env.PORT || 1337);
 
 var server = app.listen(app.get('port'), function() {
